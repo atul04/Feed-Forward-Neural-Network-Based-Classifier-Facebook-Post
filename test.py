064599264fb3266084795f37f8b214d5cb6ca668
atul04 <atulsahay01@gmail.com>
2018-10-31 00:02:21 +0530

    all submission is of this only

--- test.py
+++ test.py
@@ -2,7 +2,7 @@
 # @Date:   2018-10-22T18:34:28+05:30
 # @Email:  atulsahay01@gmail.com
 # @Last modified by:   atul
-# @Last modified time: 2018-10-29T20:58:05+05:30
+# @Last modified time: 2018-10-30T23:18:45+05:30
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -11,14 +11,17 @@ import sys
 import os
 import math
 
-
-HIDDEN_LAYERS = 1
-HIDDEN_UNITS = 50
+HIDDEN_LAYERS = 3
+HIDDEN_UNITS = 200
 OUTPUT_UNITS = 3
 BATCH_SIZE = 100
-LAMBDA = 100
-LEARNING_RATE = 0.001
+LAMBDA = 0
+LEARNING_RATE = 0.01
 EPOCHS = 1000
+ACTIVATION_FUNCTIONS = ["sigmoid","tanh","relu","softplus"]
+ACTIVATION = ACTIVATION_FUNCTIONS[0]
+EPSILON = 0.000000000001
+DROPOUT = 0
 
 n_weights_count = 0
 ################ For One Hot encoding of the values ##########################
@@ -129,11 +132,34 @@ def initialize_network(X,n_o_neurons,hidden_units,n_h_layers):
 def activate_sigmoid(sum):
     return (1/(1+np.exp(-sum)))
 
+def activate_tanh(sum):
+    return np.tanh(sum)
+
+def activate_relu(sum):
+    return sum.clip(0)
+
+def activate_softplus(sum):
+    return np.log(1+np.exp(sum))
+
+
+def dropout_activation(activation):
+    # print(activation)
+    size = len(activation)
+    # print(size)
+    indices = np.random.choice(np.arange(0,size),int(size*DROPOUT),replace=False)
+    # print(indices)
+    activation[indices] = EPSILON
+    # print(activation)
+    return activation
+
+
 def forward_propagation(net,input):
+    global ACTIVATION
     row=input
-    for layer in net:
+    for index,layer in enumerate(net):
         prev_input=np.array([])
         # print(layer)
+        # print("layer ",index+1)
         neuron = []
         for n in layer:
             neuron.append(n['weights'])
@@ -149,8 +175,16 @@ def forward_propagation(net,input):
 
         sum = neuron.dot(row)
         # print(sum)
-        result = activate_sigmoid(sum)
+        if(ACTIVATION=="sigmoid" or index == (len(net)-1)):
+            result = activate_sigmoid(sum)
+        elif(ACTIVATION=="tanh"):
+            result = activate_tanh(sum)
+        elif(ACTIVATION=="relu"):
+            result = activate_relu(sum)
+        elif(ACTIVATION=="softplus"):
+            result = activate_softplus(sum)
         # print(result)
+        result = dropout_activation(result)
 
         for index,neuron in enumerate(layer):
             neuron['result'] = result[index]
@@ -167,64 +201,82 @@ def forward_propagation(net,input):
         #
         #     prev_input=np.append(prev_input,[result])
         # row =prev_input
-
+    # exit()
     return row
 
 def sigmoidDerivative(output):
     return output*(1.0-output)
 
+def tanhDerivative(output):
+    return (1-output**2)
 
+def reluDerivative(output):
+    der = output.copy()
+    der[der>0] = 1
+    der[der<0] = 0
+    return der
+
+def softplusDerivative(output):
+    return activate_sigmoid(output)
 
 ################# Need to perform square loss
 
 def back_propagation(net,row,expected):
-     for i in reversed(range(len(net))):
-            layer=net[i]
-            errors=np.array([])
-            if i==len(net)-1:
-                results=[neuron['result'] for neuron in layer]
-                # for mse###errors = expected-np.array(results)
-                errors = np.array(results) - expected
-                for j in range(len(layer)):
-                    neuron=layer[j]
-                    # print("errors[j]",errors[j])
-                    # print("result",neuron['result'])
-                    #for mse### neuron['delta']=errors[j]*sigmoidDerivative(neuron['result'])
-                    neuron['delta']=errors[j]
-                    # print(neuron['delta'])
-            else:
-                # for j in range(len(layer)):
-                #     herror=0
-                #     nextlayer=net[i+1]
-                #     for neuron in nextlayer:
-                #         herror+=(neuron['weights'][j]*neuron['delta'])
-                #     errors=np.append(errors,[herror])
-                nextlayer = net[i+1]
-                delta = np.array([])
-                errors = []
-                for neuron in nextlayer:
-                    # print(neuron['weights'])
-                    errors.append(neuron['weights'])
-                    delta = np.append(delta,neuron['delta'])
-                errors = np.array(errors)
-                # errors = np.squeeze(errors)
-                # print(errors.shape,delta.shape)
-                errors = np.dot(errors.T,delta)
-                # print(errors.shape)
-                # exit()
-                results=[neuron['result'] for neuron in layer]
-                results = np.array(results)
-                # print(errors.shape,results.shape)
-                final_error = errors*results
-                # print(final_error.shape)
-                # exit()
-                for j in range(len(layer)):
-                    neuron=layer[j]
-                    # print("errors[j]",errors[j])
-                    # print("result",neuron['result'])
-                    #for mse### neuron['delta']=errors[j]*sigmoidDerivative(neuron['result'])
-                    neuron['delta']=final_error[j]
-                    # print(neuron['delta'])
+    global ACTIVATION
+    for i in reversed(range(len(net))):
+        layer=net[i]
+        errors=np.array([])
+        if i==len(net)-1:
+            results=[neuron['result'] for neuron in layer]
+            # for mse###errors = expected-np.array(results)
+            errors = np.array(results) - expected
+            for j in range(len(layer)):
+                neuron=layer[j]
+                # print("errors[j]",errors[j])
+                # print("result",neuron['result'])
+                #for mse### neuron['delta']=errors[j]*sigmoidDerivative(neuron['result'])
+                neuron['delta']=errors[j]
+                # print(neuron['delta'])
+        else:
+            # for j in range(len(layer)):
+            #     herror=0
+            #     nextlayer=net[i+1]
+            #     for neuron in nextlayer:
+            #         herror+=(neuron['weights'][j]*neuron['delta'])
+            #     errors=np.append(errors,[herror])
+            nextlayer = net[i+1]
+            delta = np.array([])
+            errors = []
+            for neuron in nextlayer:
+                # print(neuron['weights'])
+                errors.append(neuron['weights'])
+                delta = np.append(delta,neuron['delta'])
+            errors = np.array(errors)
+            # errors = np.squeeze(errors)
+            # print(errors.shape,delta.shape)
+            errors = np.dot(errors.T,delta)
+            # print(errors.shape)
+            # exit()
+            results=[neuron['result'] for neuron in layer]
+            results = np.array(results)
+            # print(errors.shape,results.shape)
+            if(ACTIVATION == "sigmoid"):
+                final_error = errors*sigmoidDerivative(results)
+            elif(ACTIVATION == "tanh"):
+                final_error = errors*tanhDerivative(results)
+            elif(ACTIVATION == "relu"):
+                final_error = errors*reluDerivative(results)
+            elif(ACTIVATION == "softplus"):
+                final_error = errors*softplusDerivative(results)
+            # print(final_error.shape)
+            # exit()
+            for j in range(len(layer)):
+                neuron=layer[j]
+                # print("errors[j]",errors[j])
+                # print("result",neuron['result'])
+                #for mse### neuron['delta']=errors[j]*sigmoidDerivative(neuron['result'])
+                neuron['delta']=final_error[j]
+                # print(neuron['delta'])
 
 
 def updateWeights(net,input,lrate,lam):
@@ -261,25 +313,15 @@ def updateWeights(net,input,lrate,lam):
 
 def get_mini_batch(batch_size,X,y):
     indices = list(np.random.randint(0,len(X),batch_size))
-    # x_mini = []
-    # y_mini = np.array([])
-    # for i in indices:
-    #     x_mini.append(X[i])
-    #     y_mini = np.append(y_mini,y[i])
-    # x_mini = np.array(x_mini)
-    # print(x_mini)
-    # print(y_mini)
     x_mini = np.array([])
     y_mini = np.array([])
     x_mini = X[indices]
     y_mini = y[indices]
-    # print(x_mini)
-    # print(y_mini)
-    # exit()
     return x_mini,y_mini
 
 def cross_entropy(expected,output):
-    result = -(expected*np.log(output)+(1-expected)*np.log(1-output))
+    global EPSILON
+    result = -(expected*np.log(output+EPSILON)+(1-expected)*np.log(1-output+EPSILON))
     return result
 
 def regularization(net,lam):
@@ -297,8 +339,18 @@ def regularization(net,lam):
 
     return reg
 
-def training(X,net, epochs,lrate,y,batch_size,lam):
+def decay_learn(loss,lrate,min_loss):
+    if min_loss is None:
+        return lrate, loss
+    if(loss<min_loss):
+        lrate=lrate/(1+np.abs(min_loss-loss))
+        min_loss = loss
+    return lrate,min_loss
+
+def training(X,net, epochs,lrate,y,batch_size,lam,x_valid,y_valid):
     errors=[]
+
+    min_loss = None
     for epoch in range(epochs):
         sum_error=0
         x_mini,y_mini = get_mini_batch(batch_size,X,y)
@@ -318,7 +370,6 @@ def training(X,net, epochs,lrate,y,batch_size,lam):
             # if(i%100==0):
             back_propagation(net,row,expected)
             updateWeights(net,row,lrate,lam)
-
         # print("sum_error_back",sum_error_back)
         # sum_error_back/=100
         # if epoch%10 ==0:
@@ -327,6 +378,10 @@ def training(X,net, epochs,lrate,y,batch_size,lam):
                 print("output=",outputs)
                 print("Sum_error=",sum_error/BATCH_SIZE)
 
+
+        # loss = total_loss(net,x_valid,y_valid,lam)
+        # lrate,min_loss = decay_learn(loss,lrate,min_loss)
+        # print("loss : {} LRate : {}".format(loss,lrate))
         sum_error/=BATCH_SIZE
         print('>epoch=%d,error=%f'%(epoch,sum_error))
         errors.append(sum_error)
@@ -334,33 +389,12 @@ def training(X,net, epochs,lrate,y,batch_size,lam):
         # sum_error_back = 0
     return errors,mean_cross
 
-# errors=training(net,100000, 0.05,2,y)
-#
-# epochs=[0,1,2,3,4,5,6,7,8,9]
-# plt.plot(epochs,errors)
-# plt.xlabel("epochs in 10000's")
-# plt.ylabel('error')
-# plt.show()
-
 # Make a prediction with a network
 def predict(network, row):
     outputs = forward_propagation(network, row)
     return outputs
 
 
-
-# pred=predict(net,np.array([0,1]))
-# #output=np.argmax(pred)
-# super_threshold_indices = pred >= 0.5
-# pred[super_threshold_indices] = 1
-# super_threshold_indices2 = pred < 0.5
-# pred[super_threshold_indices2] = 0
-# pred = pred.astype('int')
-# output = np.packbits(pred)
-# print(output)
-#
-#
-# print_network(net)
 
 def generate_output(x_test, net):
 # 	# writes a file (output.csv) containing target variables in required format for Kaggle Submission.
@@ -381,7 +415,7 @@ def generate_output(x_test, net):
         # print(pred)
         y_pred = np.argmax(y_pred)+1
         df.loc[i+1] = np.array([y_pred])
-    txt = 'output_H_'+str(HIDDEN_LAYERS)+'_L_'+str(LEARNING_RATE)+'_U_'+str(HIDDEN_UNITS)+'_lam_'+str(LAMBDA)+'.txt'
+    txt = 'output_H_'+str(HIDDEN_LAYERS)+'_L_'+str(LEARNING_RATE)+'_U_'+str(HIDDEN_UNITS)+'_lam_'+str(LAMBDA)+'_ACTIVATION_'+str(ACTIVATION)+'_Drop_'+str(DROPOUT)+'.txt'
     df.to_csv(txt)
     print("Done")
 
@@ -443,7 +477,7 @@ def main():
     x_train = to_normalize(x_train)
 
     # To take validation set out in proportion of 20-80 #############################
-    indexes = int(0.70*x_train.shape[0])
+    indexes = int(0.90*x_train.shape[0])
     x_train, x_valid = x_train.iloc[:indexes], x_train.iloc[indexes:]
     y_train, y_valid = y_train.iloc[:indexes], y_train.iloc[indexes:]
 
@@ -473,7 +507,7 @@ def main():
     # print_network(net)
     print("#Weights :",n_weights_count)
     ############ Training of the network ###################3
-    errors,mean_cross=training(x_train.values,net,EPOCHS, LEARNING_RATE,y_train.values,BATCH_SIZE,LAMBDA)
+    errors,mean_cross=training(x_train.values,net,EPOCHS, LEARNING_RATE,y_train.values,BATCH_SIZE,LAMBDA,x_valid.values,y_valid.values)
     print_network(net)
     epochs=[ i for i in range(len(errors)) ]
     plt.plot(epochs,errors)
@@ -488,9 +522,9 @@ def main():
     print("Valid : Loss {} Acc {} ".format(val_loss,val_acc))
 
     ########### Prediction ###################################
-    txt = 'result_cross_H_'+str(HIDDEN_LAYERS)+'_L_'+str(LEARNING_RATE)+'_U_'+str(HIDDEN_UNITS)+'_lam_'+str(LAMBDA)+'.txt'
+    txt = 'result_cross_H_'+str(HIDDEN_LAYERS)+'_L_'+str(LEARNING_RATE)+'_U_'+str(HIDDEN_UNITS)+'_lam_'+str(LAMBDA)+'_ACTIVATION_'+str(ACTIVATION)+'_Drop_'+str(DROPOUT)+'.txt'
     f = open(txt,'a')
-    f.write('\n\nResult: Hidden layers {} H_units {} Lrate {} LAMBDA {} Epochs {} Batch Size {} Training Loss: {}\n\n'.format(HIDDEN_LAYERS,HIDDEN_UNITS,LEARNING_RATE,LAMBDA,EPOCHS,BATCH_SIZE,mean_cross))
+    f.write('\n\nResult: Activation {} Hidden layers {} H_units {} Lrate {} LAMBDA {} Epochs {} Batch Size {} Drop out {} Training Loss: {}\n\n'.format(ACTIVATION,HIDDEN_LAYERS,HIDDEN_UNITS,LEARNING_RATE,LAMBDA,EPOCHS,BATCH_SIZE,DROPOUT,mean_cross))
     f.write('\nTrain : Loss {} Acc {} \n'.format(train_loss,train_acc))
     f.write('\nValid : Loss {} Acc {} \n\n'.format(val_loss,val_acc))
     cross_entropy_loss =0
